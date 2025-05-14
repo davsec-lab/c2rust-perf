1ee78621df5fc7174a9a50ce4a7d204f8b0a7b13
Alex Gao <alex@gaofamily.org>
2025-05-14 14:47:43 -0700

    fixed script

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -5,8 +5,8 @@ NUM_RUNS=${1:-1}
 INPUT_SIZES=(100000000 200000 2000 80000000 20000000 30000)
 DIRS=(quicksort selection_sort matrix_mult merge_sort binary_search dfs)
 
-CUSTOM_RUSTC="$HOME/rust/build/x86_64-unknown-linux-gnu/stage2/bin/rustc"
-VANILLA_CARGO="$HOME/rust-vanilla-install/usr/local/bin/cargo"
+CUSTOM_RUSTC="$HOME/c_to_rust/rust-modified/build/x86_64-unknown-linux-gnu/stage2/bin/rustc"
+VANILLA_CARGO="$HOME/c_to_rust/rust-vanilla-install/usr/local/bin/cargo"
 
 for i in "${!DIRS[@]}"; do
     dir="${DIRS[i]}"
